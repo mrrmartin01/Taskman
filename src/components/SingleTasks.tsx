2418f1b9fea493422327648e31d30efdf99f364e
mrrmartin01 <Theophilusmartin123@gmail.com>
2024-05-20 23:05:42 +0100

    added picture url

--- src/components/SingleTasks.tsx
+++ src/components/SingleTasks.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import { RiCheckDoubleFill, RiDeleteBin5Fill, RiEditFill } from 'react-icons/ri'
+import { Task } from './Model';
 
-type Props = {}
+type Props = {
+    tasks: Task[];
+    task: Task;
+    setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
+}
 
-const SingleTasks = (props: Props) => {
-  return (
-    <div>SingleTasks
-        <RiCheckDoubleFill />
-        <RiDeleteBin5Fill />
-        <RiEditFill />
+const SingleTasks = ({ tasks, task, setTasks }: Props) => {
+    return (
+        <form className='flex w-[29.5%] rounded-md mt-3'>
+            <span>{task.task}</span>
+            <div className="flex-1 p-1 border-none text-md bg-[url(https://source.unsplash.com/random/1600x900/?dark)] bg-cover bg-center rounded-md">
+                <span className=''><RiEditFill /></span>
+                <span className=''><RiDeleteBin5Fill /></span>
+                <span className=''><RiCheckDoubleFill /></span>
+            </div>
+        </form>
 
-
-
-    </div>
-    
-  )
+    )
 }
 
 export default SingleTasks
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import { Task } from './Model'
+import SingleTasks from './SingleTasks';
 
-interface Props{
+interface Props {
   tasks: Task[];
   setTasks: React.Dispatch<React.SetStateAction<Task[]>>
 }
 
-const  TaskList: React.FC<Props> = ({tasks, setTasks}) => {
+const TaskList: React.FC<Props> = ({ tasks, setTasks }) => {
   return (
     <div className='flex justify-evenly w-[90%] flex-wrap'>
-      {
-        tasks.map((task) => {
-          return (
-            <div key={task.id} className=''>
-              <span>{task.task}</span>
-            </div>
-          )
-        })
-      }
+      {tasks.map((task) => (
+        <div key={task.id} className=''>
+          <SingleTasks 
+          task={task} 
+          tasks={tasks} 
+          setTasks={setTasks} />
+        </div>
+      ))}
     </div>
   )
 }
